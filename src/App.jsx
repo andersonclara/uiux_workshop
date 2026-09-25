77caefef2e7517653cf3284b240a17a88ab139d7
andersonclara <andersonclara@ufl.edu>
2026-09-25 00:56:46 -0400

    Initial workshop setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <section className="projects">
+        <h1>My Projects</h1>
+
+        <div className="projects-grid">
+          {/* We'll build our ProjectCard here! */}
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default App;
